d58c19d27c4a16cfaf18fccebedf69be4abcb3c4
Ambitorik <leh.dd@yandex.ru>
2021-11-15 20:20:50 +0300

    Task 3.1

---
+++ lesson_3.1.py
@@ -0,0 +1,19 @@
+dictionary = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+
+
+def num_translate(translation):
+    return dictionary.get(translation)
+
+
+print(num_translate(input('Введите число от one до ten: ')))
