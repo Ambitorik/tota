f1eada48afa392127670099ab8db242fa4c034ed
Ambitorik <leh.dd@yandex.ru>
2021-11-23 18:15:31 +0300

    Task 5

---
+++ HomeWork#5/lesson_5.1.py
@@ -0,0 +1,15 @@
+def odd_nums(max_mums):
+    n = 1
+    while n <= max_mums:
+        yield n
+        n += 2
+
+odd_to_15 = odd_nums(15)
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
+print(next(odd_to_15))
---
+++ HomeWork#5/lesson_5.3.py
@@ -0,0 +1,21 @@
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена'
+]
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
+
+gen = ((tutor, klass) for tutor, klass in zip(klasses, tutors))
+print(next(gen))
+print(next(gen))
+
+from itertools import zip_longest
+
+gen = ((tutor, klass) for tutor, klass in zip_longest(klasses, tutors))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
